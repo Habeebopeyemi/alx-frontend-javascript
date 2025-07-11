35e45b145035bdcd3cb7cda8b6ba1288d1f2e1ed
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-11 17:09:53 +0100

    Feat: write interface for a function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,4 +27,12 @@ const director1: Director = {
   numberOfReports: 5,
 };
 
-console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
